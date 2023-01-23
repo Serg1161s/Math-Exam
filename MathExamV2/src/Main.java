4d539109a88aac3bea3d42743ece1cf80410cd5f
Sergey Bernatovich <sergeybernatovich@yandex.ru>
2023-01-23 19:30:18 +0300

    Start page

--- MathExamV2/src/Main.java
+++ MathExamV2/src/Main.java
@@ -4,6 +4,6 @@ import pages.LoginPage;
 public class Main {
     public static void main(String[] args) {
         LoginPage login = new LoginPage();
-
+        login.loginPage();
     }
 }
--- MathExamV2/src/pages/LoginPage.java
+++ MathExamV2/src/pages/LoginPage.java
@@ -2,13 +2,77 @@ package pages;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class LoginPage {
+import static java.awt.FlowLayout.CENTER;
 
-    JTextField login = new JTextField();
-    JLabel loginLabel = new JLabel();
+public class LoginPage extends JFrame {
+    JFrame frame = new JFrame("Login Page");
+    final  JTextField login = new JTextField();
+    JLabel loginLabel = new JLabel("Name");
+    JButton enter = new JButton("Enter");
+    Font loginFont = new Font(Font.SERIF,Font.PLAIN,25);
 
-    Font loginFont = new Font(null,Font.PLAIN,20);
+     public void loginPage (){
 
-    Frame frame = new Frame("Login Page");
+        frame.setBackground(Color.LIGHT_GRAY);
+        frame.setSize(300,200);
+        frame.setTitle("Login Page");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+
+        login.setFont(loginFont);
+        login.setBounds(100,30,150,50);
+        login.setHorizontalAlignment(JTextField.CENTER);
+
+
+        loginLabel.setFont(loginFont);
+        loginLabel.setBounds(20,30,70,50);
+        loginLabel.setHorizontalAlignment(JLabel.CENTER);
+        loginLabel.setVerticalTextPosition(JLabel.CENTER);
+        loginLabel.setEnabled(false);
+
+        enter.setFont(loginFont);
+        enter.setBounds(150,100,100,50);
+        enter.setHorizontalAlignment(SwingConstants.CENTER);
+
+        enter.setFocusable(false);
+
+
+
+        frame.add(login);
+        frame.add(loginLabel);
+        frame.add(enter);
+         login.addKeyListener(new KeyAdapter() {
+             @Override
+             public void keyPressed(KeyEvent e) {
+                 Pattern p = Pattern.compile("(([a-zA-Z]){0,}([\\.]){0,})+");
+                 Matcher m =p.matcher(login.getText());
+                 if(!m.matches()){
+                     login.setText("Wrong");
+                 }
+
+             }
+         });
+
+         enter.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 login.setText("");
+                 login.setEditable(true);
+             }
+         });
+
+    }
+
+
+
 }

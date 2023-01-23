d74dd72f5fddbb9a337059ece42756868843feb9
Sergey Bernatovich <Bern1161s@gmail.com>
2023-01-23 15:12:34 +0300

    start

---
+++ MathExamV2/src/Main.java
@@ -0,0 +1,9 @@
+import pages.FirstPage;
+import pages.LoginPage;
+
+public class Main {
+    public static void main(String[] args) {
+        LoginPage login = new LoginPage();
+
+    }
+}
---
+++ MathExamV2/src/pages/LoginPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginPage {
+
+    JTextField login = new JTextField();
+    JLabel loginLabel = new JLabel();
+
+    Font loginFont = new Font(null,Font.PLAIN,20);
+
+    Frame frame = new Frame("Login Page");
+}
